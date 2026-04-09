7223afeb58c84ca3f66875a354ae5a8cabaeb5f2
Vinicius Silva <vivisec007@gmail.com>
2026-04-08 22:43:24 -0300

    chore: adciona requirements e setup para empacotamento

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sistema-estoque",
+    version="1.0.0",
+    description="Sistema de Gerenciamento de Estoque – C14 Engenharia de Software",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
